6101f4521f772c08044a6627fa943e830f6dfd3a
MickeCardaba <mikael.cardaba@gmail.com>
2023-11-15 13:59:29 +0100

    created location list

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 import './../scss/style.scss'
+
+
+const locations = ["corfu", "tokyo", "vattnajökull"];
+
+class Location {
+    locationName;
+    locationCountry;
+    locationType;
+       constructor (name, country, type) {
+            this.locationName = name;
+            this.locationCountry = country;
+            this.locationType = type;
+       }
+} 
+
+
+
+for(i = 0, )
+
+const locationContainer = 
