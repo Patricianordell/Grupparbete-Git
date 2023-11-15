39b8b2c1e42da96fbf9be3a284d3d3d733b448ab
“Mona <monaelizabet@gmail.com>
2023-11-15 14:34:34 +0100

    Clothes-array added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,32 @@
-import './../scss/style.scss'
+import './../scss/style.scss';
+import { Clothes } from './models/clothes';
+
+const clothes = [];
+const item1 = new Clothes("T-shirt", "white", "122kr", "M");
+clothes.push(item1);
+const item2 = new Clothes("Shoes", "black", "1000kr", "39");
+clothes.push(item2);
+const item3 = new Clothes("Dress", "blue", "899kr", "S");
+clothes.push(item3);
+
+
+for (let i = 0; i < clothes.length; i++) {
+    const container = document.getElementById("app");
+    const itemContainer = document.createElement("div");
+    itemContainer.className = "itemContainer";
+    container.appendChild(itemContainer);
+
+    const title = document.createElement("h1");
+    const color = document.createElement("p");
+    const price = document.createElement("p");
+    const size = document.createElement("h4");
+    title.innerHTML = clothes[i].typ;
+    itemContainer.appendChild(title);
+    color.innerHTML = "Color: " + clothes[i].color;
+    itemContainer.appendChild(color);
+    price.innerHTML = "Price: " + clothes[i].price;
+    itemContainer.appendChild(price);
+    size.innerHTML = "Size: " + clothes[i].size;
+    itemContainer.appendChild(size);
+}
+
---
+++ src/js/models/clothes.js
@@ -0,0 +1,8 @@
+export class Clothes {
+    constructor(typ, color, price, size) {
+        this.typ = typ;
+        this.color = color;
+        this.price = price;
+        this.size = size;
+    }
+}
